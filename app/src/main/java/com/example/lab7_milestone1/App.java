4ee7654bf633bba2e166c7fcd98069ba9f26b573
gdong6 <gdong6@wisc.edu>
2021-11-08 11:27:01 -0600

    finish milestone1

---
+++ app/src/main/java/com/example/lab7_milestone1/App.java
@@ -0,0 +1,47 @@
+package com.example.lab7_milestone1;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+
+public class App extends Application {
+    public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_2_ID = "channel2";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannels();
+        createNotificationChannels2();
+    }
+
+    private void createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_1_ID, "channel 1", NotificationManager.IMPORTANCE_HIGH
+            );
+
+            channel1.setDescription("This is channel 1");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+        }
+    }
+
+    private void createNotificationChannels2() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel2 = new NotificationChannel(
+                    CHANNEL_2_ID, "channel 2", NotificationManager.IMPORTANCE_LOW
+            );
+
+            channel2.setDescription("This is channel 2");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel2);
+        }
+    }
+
+
+}
